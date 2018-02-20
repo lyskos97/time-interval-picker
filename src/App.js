320d10da4413c5776b0d6bf7051219815a2cc3cf
Konstantin Lyssenko <lyskos97@gmail.com>
2018-02-20 13:43:02 +0600

    refactor: `isSelected()` & `isActive()` -> `isDisabled()`

--- src/App.js
+++ src/App.js
@@ -19,7 +19,10 @@ export default class App extends React.Component<Props, void> {
         timeStep={30}
         timeMin={new Date(2018, 0, 1, 9)}
         timeMax={new Date(2018, 0, 1, 19)}
-        selectedTime={[[new Date(2018, 0, 1, 15, 30), new Date(2018, 0, 1, 15, 59)]]}
+        selectedTime={[
+          [new Date(2018, 0, 1, 15, 30), new Date(2018, 0, 1, 15, 59)],
+          [new Date(2018, 0, 1, 16, 0), new Date(2018, 0, 1, 16, 29)],
+        ]}
         busyTime={[[new Date(2018, 0, 1, 12, 30), new Date(2018, 0, 1, 13, 30)]]}
         onChange={this.cb}
       />
--- src/components/TimeIntervalPicker/TimeIntervalPicker.js
+++ src/components/TimeIntervalPicker/TimeIntervalPicker.js
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-array-index-key */
 
 import * as React from 'react';
-import { differenceInMinutes, addMinutes } from 'date-fns';
+import { differenceInMinutes, addMinutes, isWithinRange } from 'date-fns';
 import Timestamp from './Timestamp';
 
 export type DateRange = [Date, Date];
@@ -32,47 +32,64 @@ export default class TimeIntervalPicker extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
 
-    const { timeMin, timeMax, timeStep } = props;
+    this.state = { selectedTime: props.selectedTime, stamps: [] };
 
-    const diffInMinutes = differenceInMinutes(timeMax, timeMin);
-    const stampsNo = diffInMinutes / timeStep;
+    const stamps = this.getStampsFromProps();
+
+    // double assigning for proper work of `this.isSelected()`
+    this.state = { stamps, selectedTime: props.selectedTime };
+  }
+
+  getStampsFromProps(): Array<Stamp> {
+    const { timeMin, timeMax, timeStep } = this.props;
     const stamps: Array<Stamp> = [];
+    const diffInMinutes = differenceInMinutes(timeMax, timeMin);
+    const stampsNumber = diffInMinutes / timeStep;
 
-    for (let i = 0; i <= stampsNo; i++) {
+    for (let i = 0; i < stampsNumber; i++) {
       const nextDate = addMinutes(timeMin, timeStep * i);
 
       if (this.isSelected(nextDate)) {
         stamps.push({ value: nextDate, status: 'selected' });
-      } else if (this.isActive(nextDate)) {
+      } else if (this.isDisabled(nextDate)) {
         stamps.push({ value: nextDate, status: 'available' });
       } else {
         stamps.push({ value: nextDate, status: 'disabled' });
       }
     }
 
-    this.state = { stamps, selectedTime: props.selectedTime };
+    return stamps;
   }
 
-  // TODO: merge? `isActive()` and `isSelected()` into `belongsTo()?`
-  isActive(nextDate: Date): boolean {
+  isDisabled(nextDate: Date): boolean {
     const { busyTime } = this.props;
-    let active = true;
+    let disabled = false;
 
     if (busyTime) {
-      busyTime.forEach(elArr => {
-        const [firstEl, secondEl] = elArr;
-
-        if (nextDate.getTime() >= firstEl.getTime() && nextDate.getTime() <= secondEl.getTime()) {
-          active = false;
-        }
+      busyTime.forEach(datePair => {
+        if (isWithinRange(nextDate, ...datePair)) disabled = true;
       });
     }
 
-    return active;
+    return disabled;
   }
 
   isSelected(nextDate: Date): boolean {
-    const { selectedTime } = this.props;
+    const { selectedTime } = this.state;
+    let selected = false;
+
+    if (selectedTime) {
+      selectedTime.forEach(datePair => {
+        if (isWithinRange(nextDate, ...datePair)) selected = true;
+      });
+    }
+
+    return selected;
+  }
+
+  // TODO: remove redundant
+  isSelectedState(nextDate: Date): boolean {
+    const { selectedTime } = this.state;
     let selected = false;
 
     if (selectedTime) {
@@ -109,22 +126,33 @@ export default class TimeIntervalPicker extends React.Component<Props, State> {
     });
   }
 
-  // getStampAdjacency(stamp: Stamp): -1 | 0 | 1 | 2 {
-  //   const { selectedTime } = this.state;
+  onStampClick2(i: number) {
+    const { selectedTime = [], stamps } = this.state;
+    const { onChange, timeStep } = this.props;
+    // const cbTime = selectedTime.slice();
 
-  //   selectedTime.forEach(el => {
-  //     differenceInMinutes();
-  //   });
+    if (stamps[i].status === 'available') {
+      stamps[i].status = 'selected';
+      selectedTime.push([stamps[i].value, addMinutes(stamps[i].value, timeStep - 1)]);
+    } else if (stamps[i].status === 'selected') {
+      stamps[i].status = 'available';
+      const selectedIndex = selectedTime.findIndex(() => this.isSelected(stamps[i].value));
+      console.log('selectedTime before', selectedTime);
+      selectedTime.splice(selectedIndex, 0);
+      console.log('selectedTime after', selectedTime);
+    } else return;
 
-  //   return 0;
-  // }
+    this.setState({ stamps, selectedTime }, () => {
+      if (onChange) onChange(selectedTime);
+    });
+  }
 
   render() {
     const { stamps } = this.state;
 
     return (
       <div className="wrapper">
-        <h3 className="picker-header">React timestamps picker</h3>
+        <h3 className="picker-header">React timespan picker</h3>
         <div className="picker">
           {stamps.map((el, i) => (
             <Timestamp
@@ -132,7 +160,7 @@ export default class TimeIntervalPicker extends React.Component<Props, State> {
               value={el.value}
               status={el.status}
               handleClick={() => {
-                this.onStampClick(i);
+                this.onStampClick2(i);
               }}
             />
           ))}
